bc782a8dfc98b73bd5d217c13bae46ec68c51963
Almielka <anna.s.almielka@gmail.com>
2020-12-10 19:12:12 +0100

    Refactoring: Сreating new objects in a loop

--- src/main/java/year2020/Application.java
+++ src/main/java/year2020/Application.java
@@ -2,6 +2,8 @@ package year2020;
 
 import year2020.days.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,17 +12,21 @@ import java.util.List;
  */
 
 public class Application {
-    private static final List<AbstractDay> DAYS = List.of(
-            new Day01(),
-            new Day02(),
-            new Day03(),
-            new Day04()
-    );
 
-    public static void main(String[] args) {
-        DAYS.forEach(day -> {
+    public static void main(String[] args) throws InstantiationException, InvocationTargetException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException {
+
+        List<AbstractDay> days = new ArrayList<>();
+
+        for (int i = 1; i <= 4; i++) { //i will be increased up to 25
+            String day = (i < 10) ? "0" + i : String.valueOf(i);
+            days.add((AbstractDay) Class.forName("year2020.days.Day" + day).getConstructor().newInstance());
+        }
+
+        days.forEach(day -> {
             System.out.println("Day " + day.getDay() + ", Part 1: " + day.solvePart1());
             System.out.println("Day " + day.getDay() + ", Part 2: " + day.solvePart2());
         });
+
     }
+
 }
