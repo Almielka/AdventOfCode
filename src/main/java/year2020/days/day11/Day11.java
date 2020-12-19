0a28ba5d3be10543f4bd65d456eba9c3ee24d264
Almielka <anna.s.almielka@gmail.com>
2020-12-19 21:28:26 +0100

    Solution Day 11: Seating System, Part 2

--- src/main/java/year2020/days/day11/Day11.java
+++ src/main/java/year2020/days/day11/Day11.java
@@ -16,6 +16,8 @@ public class Day11 extends AbstractDay {
     private static final char FLOOR = '.';
     private static Seat[][] seatingArea;
     private static int countOccupied;
+    private static final int ADJACENT = 4;
+    private static final int VISIBLE = 5;
 
     public Day11() {
         super(DAY);
@@ -23,17 +25,18 @@ public class Day11 extends AbstractDay {
 
     @Override
     public String solvePart1() {
-        return String.valueOf(occupySeatingArea(getSeatingArea(getStringFromInput(DAY))));
+        return String.valueOf(occupySeatingArea(getSeatingArea(getStringFromInput(DAY)), ADJACENT));
     }
 
     @Override
     public String solvePart2() {
-        return String.valueOf(0);
+        return String.valueOf(occupySeatingArea(getSeatingArea(getStringFromInput(DAY)), VISIBLE));
     }
 
     private Seat[][] getSeatingArea(String str) {
         String[] arr = str.split("\r\n");
         seatingArea = new Seat[arr.length][arr[0].length()];
+        countOccupied = 0;
         for (int x = 0; x < arr.length; x++) {
             String line = arr[x];
             for (int y = 0; y < line.length(); y++) {
@@ -47,14 +50,14 @@ public class Day11 extends AbstractDay {
         return seatingArea;
     }
 
-    private int occupySeatingArea(Seat[][] seatingArea) {
+    private int occupySeatingArea(Seat[][] seatingArea, int value) {
         int checkSeat = seatingArea.length * seatingArea[0].length;
         for (Seat[] seatsRow : seatingArea) {
             for (Seat currentSeat : seatsRow) {
                 if (currentSeat.isSeat && !currentSeat.isOccupied && currentSeat.countNeighbors == 0) {
                     currentSeat.isOccupied = true;
                     countOccupied++;
-                } else if (currentSeat.isSeat && currentSeat.isOccupied && currentSeat.countNeighbors >= 4) {
+                } else if (currentSeat.isSeat && currentSeat.isOccupied && currentSeat.countNeighbors >= value) {
                     currentSeat.isOccupied = false;
                     countOccupied--;
                 } else {
@@ -62,34 +65,56 @@ public class Day11 extends AbstractDay {
                 }
             }
         }
-        countOccupied();
-        if (checkSeat != 0) occupySeatingArea(seatingArea);
+        countOccupied(value);
+        if (checkSeat != 0) occupySeatingArea(seatingArea, value);
         return countOccupied;
     }
 
-    private void countOccupied() {
+    private void countOccupied(int value) {
         for (Seat[] seatsRow : seatingArea) {
             for (Seat currentSeat : seatsRow) {
                 if (currentSeat.isSeat) {
-                    currentSeat.countNeighbors = getNeighbors(currentSeat).size();
+                    if (value == ADJACENT) currentSeat.countNeighbors = getAdjacentNeighbors(currentSeat);
+                    else if (value == VISIBLE) currentSeat.countNeighbors = getVisibleNeighbors(currentSeat);
                 }
             }
         }
     }
 
-    private ArrayList<Seat> getNeighbors(Seat seat) {
-        ArrayList<Seat> listNeighbors = new ArrayList<>();
+    private int getAdjacentNeighbors(Seat seat) {
+        int result = 0;
         for (int x = seat.x - 1; x <= seat.x + 1; x++) {
             for (int y = seat.y - 1; y <= seat.y + 1; y++) {
-                try {
-                    if (!seatingArea[x][y].equals(seat) && seatingArea[x][y].isOccupied) {
-                        listNeighbors.add(seatingArea[x][y]);
-                    }
-                } catch (ArrayIndexOutOfBoundsException ignored) {
-                }
+                if (!isInArea(x, y)) continue;
+                if (!seatingArea[x][y].equals(seat) && seatingArea[x][y].isOccupied) result++;
             }
         }
-        return listNeighbors;
+        return result;
+    }
+
+    private boolean isInArea(int x, int y) {
+        return x >= 0 && x < seatingArea.length && y >= 0 && y < seatingArea[x].length;
+    }
+
+    private int getVisibleNeighbors(Seat seat) {
+        int result = 0;
+        for (int rangeX = -1; rangeX <= 1; rangeX++) {
+            for (int rangeY = -1; rangeY <= 1; rangeY++) {
+                if (rangeX == 0 && rangeY == 0) continue;
+                if (hasVisibleOccupiedSeat(seat.x, seat.y, rangeX, rangeY)) result++;
+            }
+        }
+        return result;
+    }
+
+    boolean hasVisibleOccupiedSeat(int x, int y, int rangeX, int rangeY) {
+        do {
+            x += rangeX;
+            y += rangeY;
+        }
+        while (isInArea(x, y) && !seatingArea[x][y].isSeat);
+
+        return isInArea(x, y) && seatingArea[x][y].isOccupied;
     }
 
     private static class Seat {
